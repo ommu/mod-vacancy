7114570704f463075ff05bcef34d8bfc1b2fda0d
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2018-01-22 19:08:32 +0700

    add composer and modify module config

--- VacancyModule.php
+++ VacancyModule.php
@@ -2,6 +2,9 @@
 
 class VacancyModule extends CWebModule
 {
+	public $publicControllers = array();
+	private $_module = 'vacancy';
+
 	public $defaultController = 'site'; 
 	
 	// getAssetsUrl()
@@ -16,31 +19,40 @@ class VacancyModule extends CWebModule
 		// import the module-level models and components
 		$this->setImport(array(
 			'vacancy.models.*',
+			'vacancy.models.view.*',
 			'vacancy.components.*',
 		));
-	}
- 
-	public function getAssetsUrl()
-	{
-		if ($this->_assetsUrl === null)
-			$this->_assetsUrl = Yii::app()->getAssetManager()->publish(Yii::getPathOfAlias('vacancy.assets'));
-		
-		return $this->_assetsUrl;
+
+		// this method is called before any module controller action is performed
+		// you may place customized code here
+		// list public controller in this module
+		$publicControllers = array();
+		$controllerMap = array();
+
+		$controllerPath = Yii::getPathOfAlias('application.modules.'.$this->_module.'.controllers');
+		foreach (new DirectoryIterator($controllerPath) as $fileInfo) {
+			if($fileInfo->isDot())
+				continue;
+			
+			if($fileInfo->isFile() && !in_array($fileInfo->getFilename(), array('index.php'))) {
+				$getFilename = $fileInfo->getFilename();
+				$publicControllers[] = $controller = strtolower(preg_replace('(Controller.php)', '', $getFilename));
+				$controllerMap[$controller] = array(
+					'class'=>'application.modules.'.$this->_module.'.controllers.'.preg_replace('(.php)', '', $getFilename),
+				);
+			}
+		}
+		$this->controllerMap = $controllerMap;
+		$this->publicControllers = $publicControllers;
 	}
 
-	public function beforeControllerAction($controller, $action) {
-		if(parent::beforeControllerAction($controller, $action)) {
-			// this method is called before any module controller action is performed
-			// you may place customized code here
-			//list public controller in this module
-			$publicControllers = array(
-				'site',
-				'user',
-			);
-			
+	public function beforeControllerAction($controller, $action) 
+	{
+		if(parent::beforeControllerAction($controller, $action)) 
+		{
 			// pake ini untuk set theme per action di controller..
 			// $currentAction = Yii::app()->controller->id.'/'.$action->id;
-			if(!in_array(strtolower(Yii::app()->controller->id), $publicControllers) && !Yii::app()->user->isGuest) {
+			if(!in_array(strtolower(Yii::app()->controller->id), $this->publicControllers) && !Yii::app()->user->isGuest) {
 				$arrThemes = Utility::getCurrentTemplate('admin');
 				Yii::app()->theme = $arrThemes['folder'];
 				$this->layout = $arrThemes['layout'];
@@ -51,5 +63,13 @@ class VacancyModule extends CWebModule
 		}
 		else
 			return false;
+	}
+ 
+	public function getAssetsUrl()
+	{
+		if ($this->_assetsUrl === null)
+			$this->_assetsUrl = Yii::app()->getAssetManager()->publish(Yii::getPathOfAlias('vacancy.assets'));
+		
+		return $this->_assetsUrl;
 	}
 }
